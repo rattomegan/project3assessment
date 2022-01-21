e1e902f1f75cf9a91a015c92e2294637abed18d6
Megan Ratto <meganratto@Megans-MacBook-Air.local>
2022-01-21 12:10:53 -0800

    Complete Step 3.3

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-  path('', views.widgets_index, name='widgets_index')
+  path('', views.widgets_index, name='widgets_index'),
+  path('addwidget', views.WidgetCreate.as_view(), name='add_widget'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from .models import Widget
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 def widgets_index(request):
   widgets = Widget.objects.all
   return render(request, 'index.html', { 'widgets': widgets })
+
+class WidgetCreate(CreateView):
+  model = Widget
+  fields = '__all__'
+  success_url = '/'
+
